1313e4b99a04b2b4a03324d91fec4e48225213c9
chriscors <70537376+chriscors@users.noreply.github.com>
2023-06-14 22:29:43 -0400

    Adds imports for app

--- tmdb-vite/src/App.tsx
+++ tmdb-vite/src/App.tsx
@@ -1,4 +1,10 @@
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import "./App.css";
+import Sidebar from "./components/sidebar";
+import MovieCard from "./components/movie-card";
+import { CssBaseline, createTheme, useMediaQuery } from "@mui/material";
+import { useMemo } from "react";
+import { ThemeProvider } from "@emotion/react";
 
 function App() {
   return <></>;
