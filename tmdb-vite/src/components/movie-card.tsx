4d2638287aed1d35ac14a9099aeb2d3b0ba05797
chriscors <70537376+chriscors@users.noreply.github.com>
2023-06-15 17:14:51 -0400

    Adds basic card structure

--- tmdb-vite/src/components/movie-card.tsx
+++ tmdb-vite/src/components/movie-card.tsx
@@ -1,5 +1,28 @@
-import { Card } from "@mui/material";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
 export default function MovieCard() {
-  return <Card></Card>;
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Grid2 container sx={{ flexDirection: "column" }}>
+          <Typography gutterBottom variant="h5" component="div">
+            Movie Title
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+            Movie Rating
+          </Typography>
+        </Grid2>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
