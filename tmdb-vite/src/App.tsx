3501965d786e27605c7e7ff5c0ea552e6aebed1e
chriscors <70537376+chriscors@users.noreply.github.com>
2023-06-14 22:30:38 -0400

    Adds dark theme detector and basic grid outline

--- tmdb-vite/src/App.tsx
+++ tmdb-vite/src/App.tsx
@@ -7,7 +7,40 @@ import { useMemo } from "react";
 import { ThemeProvider } from "@emotion/react";
 
 function App() {
-  return <></>;
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    // Enable dark mode using the theme provider component
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {/* Outer level grid */}
+      <Grid2 container spacing={2}>
+        {/* Sidebar */}
+        <Grid2 xs={12} sm={4}>
+          <Sidebar />
+        </Grid2>
+        {/* Results */}
+        <Grid2 xs={12} sm={8}>
+          <div className="flex">Main!</div>
+          <Cards></Cards>
+        </Grid2>
+      </Grid2>
+    </ThemeProvider>
+  );
+}
+
+function Cards() {
+  return <MovieCard></MovieCard>;
 }
 
 export default App;
