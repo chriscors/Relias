7f06c3ce75026248ee80ba8621195821d08976bf
chriscors <70537376+chriscors@users.noreply.github.com>
2023-06-16 00:00:37 -0400

    Adds test movies

--- tmdb-vite/src/App.tsx
+++ tmdb-vite/src/App.tsx
@@ -2,7 +2,12 @@ import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import "./App.css";
 import Sidebar from "./components/sidebar";
 import MovieCard from "./components/movie-card";
-import { CssBaseline, createTheme, useMediaQuery } from "@mui/material";
+import {
+  CssBaseline,
+  Typography,
+  createTheme,
+  useMediaQuery,
+} from "@mui/material";
 import { useMemo } from "react";
 import { ThemeProvider } from "@emotion/react";
 
@@ -31,8 +36,10 @@ function App() {
         </Grid2>
         {/* Results */}
         <Grid2 xs={12} sm={8}>
-          <div className="flex">Main!</div>
-          <Cards></Cards>
+          <Typography variant="h4" marginBottom={"1rem"}>
+            Results
+          </Typography>
+          <Cards />
         </Grid2>
       </Grid2>
     </ThemeProvider>
@@ -40,7 +47,13 @@ function App() {
 }
 
 function Cards() {
-  return <MovieCard></MovieCard>;
+  return (
+    <Grid2 container spacing={3} justifyContent={"center"}>
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+    </Grid2>
+  );
 }
 
 export default App;
