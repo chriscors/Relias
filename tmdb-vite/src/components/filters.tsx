f74ebb764d38ebc791572a2028c66c0978c0ea09
chriscors <70537376+chriscors@users.noreply.github.com>
2023-06-19 15:05:35 -0400

    removes select change

--- tmdb-vite/src/components/filters.tsx
+++ tmdb-vite/src/components/filters.tsx
@@ -4,7 +4,7 @@ import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Select from "@mui/material/Select";
 import Chip from "@mui/material/Chip";
 import axios from "axios";
 import { Genre } from "../types";
