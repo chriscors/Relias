d53566b059d0161785ff31fa8e5d8c573f112d3f
chriscors <70537376+chriscors@users.noreply.github.com>
2023-06-19 11:25:04 -0400

    Adds checks for data availability

--- tmdb-vite/src/components/movie-card.tsx
+++ tmdb-vite/src/components/movie-card.tsx
@@ -3,24 +3,10 @@ import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import "../App.css";
 import StarHalfIcon from "@mui/icons-material/StarHalf";
 import { Star, StarBorder, StarBorderOutlined } from "@mui/icons-material";
-
+import { MovieData } from "../types";
+import TheatersIcon from "@mui/icons-material/Theaters";
 interface MovieDataProps {
-  movieData: {
-    adult: boolean;
-    backdrop_path: string;
-    genre_ids: number[];
-    id: number;
-    original_language: string;
-    original_title: string;
-    overview: string;
-    popularity: number;
-    poster_path: string;
-    release_date: string;
-    title: string;
-    video: boolean;
-    vote_average: number;
-    vote_count: number;
-  };
+  movieData: MovieData;
 }
 
 export default function MovieCard({ movieData }: MovieDataProps) {
@@ -38,8 +24,12 @@ export default function MovieCard({ movieData }: MovieDataProps) {
         <CardMedia
           //Movie poster
           sx={{ height: 340 }}
-          image={`https://image.tmdb.org/t/p/original${movieData.poster_path}`}
-          className="description"
+          image={
+            movieData.poster_path
+              ? `https://image.tmdb.org/t/p/original${movieData.poster_path}`
+              : "../film.png"
+          }
+          className={`${!movieData.poster_path && "no-poster"} description`}
         >
           <Grid2
             //Container for the description
@@ -84,7 +74,9 @@ export default function MovieCard({ movieData }: MovieDataProps) {
             </Grid2>
           </Grid2>
           <Grid2 container justifyContent={"space-between"}>
-            <Typography variant="h6">{releaseDate.getFullYear()}</Typography>
+            <Typography variant="h6">
+              {movieData.release_date && releaseDate.getFullYear()}
+            </Typography>
             <Grid2
               xs={3}
               container
@@ -92,11 +84,11 @@ export default function MovieCard({ movieData }: MovieDataProps) {
               alignContent={"flex-start"}
             >
               <Typography gutterBottom variant="h6" sx={{ margin: "auto 0" }}>
-                {movieData.vote_average.toFixed(1)}
+                {movieData?.vote_average && movieData.vote_average.toFixed(1)}
               </Typography>
-              {movieData.vote_average < 3 ? (
+              {movieData?.vote_average && movieData.vote_average < 3 ? (
                 <StarBorderOutlined sx={{ height: "100%" }} />
-              ) : movieData.vote_average < 7 ? (
+              ) : movieData?.vote_average && movieData.vote_average < 7 ? (
                 <StarHalfIcon sx={{ height: "100%" }} />
               ) : (
                 <Star sx={{ height: "100%" }} />
